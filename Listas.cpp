b3cd03d5cdf2767912651df3615d444a6e8e45fc
vicjuice28 <vicjuice28@github.com>
2020-11-17 15:34:24 -0700

    Push front y pop front funcionando,funciones declaradas

--- Listas.cpp
+++ Listas.cpp
@@ -8,33 +8,24 @@ int main() {
     // Lista simple enlazada
     cout<<"Lista simple enlazada"<<endl;
     Lista<int> lista;
-    lista.push(1);
-    lista.push(2);
-    lista.push(3);
+    lista.push_front(1);
+    lista.push_front(2);
+    lista.push_front(3);
     cout<<"El tamaño es: "<< lista.size()<<endl;
 
     cout<<lista.front()<<endl;
-    lista.pop();
+    lista.pop_front();
     cout<<lista.front()<<endl;
-    lista.pop();
+    lista.pop_front();
     cout<<lista.front()<<endl;
-    lista.pop();
+    lista.pop_front();
     cout<<"El tamaño es: "<<lista.size()<<endl;
 
     if(!lista.empty())
         cout<<lista.front()<<endl;
     else cout<<"Lista simple vacia."<<endl;
 
-    // Cola circular basada en arreglo
-    //ColaCircular<int> colaCircular;
-    //cout<<"Cola circular"<<endl;
-    //for(int i=0;i<100;i++) {
-     //   colaCircular.push(i);
-    //}
-   // for(int i=0;i<100;i++) {
-        //cout<<colaCircular.front()<<endl;
-        //colaCircular.pop();
-    //}
+    
     
 
     return 0;
--- listasimple.hpp
+++ listasimple.hpp
@@ -16,42 +16,48 @@ class Lista {
         Nodo *frente;
         Nodo *final;
         int tam;
+        int i;
 
     public:
         Lista(){
             frente = nullptr;
             final = nullptr;
             tam = 0;
+            i = 0;
         }
-        void push(T d);
-        void pop();
+        void push_front(T d);
+        void pop_front();
         T front();
         T back();
         bool empty();
         int size();
         void clear();
-        void push_i(T d);
-        void pop_i();
-        void update_i(T d);
-        T show_i();
+        void push(T d, int i);
+        void pop(T d, int i);
+        void set(T d, int i);
+        T get(T d, int i);
+        void push_back(T d);
+        void pop_back(T d);
         ~Lista(){
             clear();
         }
 };
-// implementacion
+// Implementacion
 template <class T>
-void Lista<T>::push(T d) {
+void Lista<T>::push_front(T d) {
     Nodo *n = new Nodo(d);
     if(empty()) {
         frente = n;
+        final = n;
     } else {
-        final->siguiente = n;
+        n->siguiente= frente;
+        frente = n;
     }
-    final = n;
+    
     tam++;
 }
 template <class T>
-void Lista<T>::pop() {
+void Lista<T>::pop_front() {
     Nodo *aux = frente;
     frente = frente->siguiente;
     delete aux;
@@ -85,29 +91,48 @@ void Lista<T>::clear() {
 }
 
 template <class T>
-void Lista<T>::push_i(T d){
+void Lista<T>::push(T d, int i){
 
     //Falta por agregar
 }
 
 template <class T>
-void Lista<T>::pop_i(){
-
-    //Falta por agregar
-}
-
-
-template <class T>
-void Lista<T>::update_i(T d){
+void Lista<T>::pop(T d, int i){
 
     //Falta por agregar
 }
 
 
 template <class T>
-T Lista<T>::show_i(){
+void Lista<T>::set(T d, int i){
 
     //Falta por agregar
+}
+
+
+template <class T>
+T Lista<T>::get(T d, int i){
+
+    //Falta por agregar
+}
+
+template <class T>
+void Lista<T>::push_back(T d){
+    Nodo *n = new Nodo(d);
+    if(empty()) {
+        frente = n;
+    } else {
+        final->siguiente = n;
+    }
+    final = n;
+    tam++;
+
+}
+
+template <class T>
+void Lista<T>::pop_back(T d){
+
+
 }
 
 #endif
