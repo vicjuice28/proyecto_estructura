28c30c7a522d2739fd75542d6b01a13fb9f90430
vicjuice28 <vicjuice28@github.com>
2021-01-17 14:43:49 -0700

    Primera version del menu, aun sin funciones

--- PELICULAS/main.cpp
+++ PELICULAS/main.cpp
@@ -1,19 +1,55 @@
 #include "store.hpp"
 
 
-menu(){
+ void menu(){
+     int choice;
+cout<<endl<<"\tBienvenido al sistema de peliculas de Movie Store"<<endl;
 
-cout<<endl<<"\tBienvenido al sistema de peliculas por Movie Store"<<endl;
+cout<<"\n\n1.Agregar peliculas\n2.Eliminar peliculas\n3.Editar información de una pelicula\n4.Mostrar todas las peliculas\n5.Ordenar peliculas por nombre\n6.Ordenar peliculas por año\n7.Mostrar peliculas por precio\n8.Cerrar programa"<<endl;
+cout<<"\n¿Que accion desea realizar? : "<<endl;cin>>choice;
+
+switch(choice){
+    case 1:
+    //Agregar pelicula
+    break;
+    case 2:
+    //Eliminar pelicula
+    break;
+    case 3:
+    //Editar info
+    break;
+    case 4:
+    //Mostrar todas las peliculas
+    break;
+    case 5:
+    //Ordenar por nombre
+    break;
+    case 6:
+    //Ordenar por año
+    break;
+    case 7:
+    //Ordenar por precio
+    break;
+    case 8:
+    //Cerrar programa
+    cout<<"\nGracias por utilizar el programa\nCerrando...."<<endl;
+    break;
+    default:
+    system("clear");
+    cout<<"\n\t\t¡¡¡DIGITE UNA OPCIÓN VALIDA!!!"<<endl;
+    
+    menu();
+
+
+
+}
 
 }
 
 
 
 
-
-
-int main()
-{
+int main(){
     Store store = Store();
 
     store.create(4, "Batman vs Superman", "It's a really good movie", 2007, 20);
@@ -24,11 +60,13 @@ int main()
     store.create(8, "It's okay", "It's a really bad movie", 2009, 120);
     store.create(9, "Chest", "It's a really bad movie", 2009, 10);
 
+    system("clear");
+    menu();
     //  //*It works!
     //store.showAll();
 
     // //*It wokrs!
-    cout << (store.find(1).getName()) ;
+    //cout << (store.find(1).getName()) ;
 
     // //*It works!
     //store.remove(1);
